31c10d515b23fe33ff83074994395c55ab9f5b89
jfrancisohalloran <161669277+jfrancisohalloran@users.noreply.github.com>
2025-01-23 10:41:57 -0600

    Timestamp and frequency = h

--- assignment1.py
+++ assignment1.py
@@ -4,12 +4,14 @@ from statsmodels.tsa.holtwinters import ExponentialSmoothing
 
 train_data = pd.read_csv(
     'assignment_data_train.csv',
-    parse_dates=['datetime'],
-    index_col='datetime'
+    parse_dates=['Timestamp'],
+    index_col='Timestamp'
 )
 
+train_data = train_data.asfreq('h')
+
 model = ExponentialSmoothing(
-    train_data['trip_count'],        
+    train_data['trips'],        
     trend='add',                     
     seasonal='add',                  
     seasonal_periods=24*7            
@@ -19,10 +21,13 @@ modelFit = model.fit()
 
 test_data = pd.read_csv(
     'assignment_data_test.csv',
-    parse_dates=['datetime'],
-    index_col='datetime'
+    parse_dates=['Timestamp'],
+    index_col='Timestamp'
 )
 
-pred = modelFit.forecast(steps=len(test_data))
+test_data = test_data.asfreq('h')
+
+steps_to_forecast = len(test_data)
+pred = modelFit.forecast(steps=steps_to_forecast)
 
 pred = pred.values
